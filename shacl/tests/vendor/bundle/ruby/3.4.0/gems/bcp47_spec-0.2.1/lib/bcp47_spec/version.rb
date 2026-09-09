7df14cfba65d2f831d10854001d37063a733832e
Gregory Saumier-Finch <gregory@culturecreates.com>
2026-09-09 13:12:05 -0400

    Revert "bump v4.8"
    This reverts commit fbe4e59d1678ab19464a8315909ceed3f7356e4d.

--- shacl/tests/vendor/bundle/ruby/3.4.0/gems/bcp47_spec-0.2.1/lib/bcp47_spec/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module BCP47
-  VERSION = '0.2.1'.freeze
-end
--- shacl/tests/vendor/bundle/ruby/3.4.0/gems/rdf-3.3.4/lib/rdf/util.rb
+++
@@ -1,8 +0,0 @@
-module RDF; module Util
-  autoload :Aliasing,  'rdf/util/aliasing'
-  autoload :Cache,     'rdf/util/cache'
-  autoload :File,      'rdf/util/file'
-  autoload :Logger,    'rdf/util/logger'
-  autoload :UUID,      'rdf/util/uuid'
-  autoload :Coercions, 'rdf/util/coercions'
-end; end
